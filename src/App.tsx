eec0c3c076a3d56aa567127bcf734451f70bc272
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-03 10:27:27 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import './App.css';
+import Header from './components/Header/Header';
+import HomePage from './components/HomePage/HomePage';
+import Footer from './components/Footer/Footer';
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <HomePage />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
