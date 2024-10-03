3fd0b2d2612808b553dd81312bb0d3625bffc4e1
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-03 23:15:18 +0200

    style article product finis

--- src/components/HomePage/Products.tsx
+++ src/components/HomePage/Products.tsx
@@ -2,33 +2,68 @@ function Products() {
   return (
     <div>
       <h1 className="px-4 pt-10 font-bold text-3xl">Tous nos produits</h1>
-      <article className="p-4 ">
+      <article className="m-4 border-Main_low ">
         <div className="relative pb-6">
           <span className=" rounded-tl-lg top-0 left-0 pl-4 pr-10 py-1  bg-Brand_primary text-sm text-Main_Lowest [clip-path:polygon(0%_0%,_100%_0%,_81%_100%,_0%_100%)]">
             N°1 des ventes
           </span>
           <img
-            className=" object-cover mx-auto "
+            className="w-1/2 object-cover mx-auto mt-6 "
             src="../src/assets/products/frisbee.webp"
             alt="Frisbee"
           />
           <div className="absolute inset-0 bg-[rgba(0,0,0,0.04)] " />
         </div>
-        <p>
-          Trixie Frisbee Caoutchouc Naturel 22cm pour chien, Coloris aléatoire
-        </p>
-        <div className="flex items-center gap-1">
-          <span className="font-bold text-3xl">8</span>
-          <sup className="font-normal text-xs  ">99 €</sup>
+
+        <div className="border-r border-b border-l border-Main_low p-4">
+          <p>
+            Trixie Frisbee Caoutchouc Naturel 22cm pour chien, Coloris aléatoire
+          </p>
+          <div className="flex items-center gap-1 mt-10 mb-4">
+            <span className="font-bold text-3xl">8</span>
+            <sup className="font-normal text-xs  ">99 €</sup>
+          </div>
+
+          <button
+            className="p-2 px-4 bg-Brand_primary rounded-3xl text-xs
+        "
+            type="button"
+          >
+            Ajouter au panier
+          </button>
+        </div>
+      </article>
+
+      <article className="m-4 border-Main_low ">
+        <div className="relative pb-6">
+          <span className=" rounded-tl-lg top-0 left-0 pl-4 pr-10 py-1  bg-Brand_primary text-sm text-Main_Lowest [clip-path:polygon(0%_0%,_100%_0%,_81%_100%,_0%_100%)]">
+            N°1 des ventes
+          </span>
+          <img
+            className="w-1/2 object-cover mx-auto mt-6 "
+            src="../src/assets/products/frisbee.webp"
+            alt="Frisbee"
+          />
+          <div className="absolute inset-0 bg-[rgba(0,0,0,0.04)] " />
         </div>
 
-        <button
-          className="p-2 bg-Brand_primary rounded-3xl
+        <div className="border-r border-b border-l border-Main_low p-4">
+          <p>
+            Trixie Frisbee Caoutchouc Naturel 22cm pour chien, Coloris aléatoire
+          </p>
+          <div className="flex items-center gap-1 mt-10 mb-4">
+            <span className="font-bold text-3xl">8</span>
+            <sup className="font-normal text-xs  ">99 €</sup>
+          </div>
+
+          <button
+            className="p-2 px-4 bg-Brand_primary rounded-3xl text-xs
         "
-          type="button"
-        >
-          Ajouter au panier
-        </button>
+            type="button"
+          >
+            Ajouter au panier
+          </button>
+        </div>
       </article>
     </div>
   );
