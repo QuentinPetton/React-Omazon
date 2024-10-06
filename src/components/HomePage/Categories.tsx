54a66c4350f3fd148136ad1bba48b4a125fa280e
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-06 21:13:19 +0200

    ajout methode .map sur categories

--- src/components/HomePage/Categories.tsx
+++ src/components/HomePage/Categories.tsx
@@ -21,17 +21,20 @@ function Categories() {
   return (
     <div>
       <h2 className="px-4 pt-10 font-bold text-3xl">Catégories à l'honneur</h2>
-      <article className="">
-        <div className="relative w-40 h-40 text-center">
-          <img
-            src="../src/assets/categories/animalerie.webp"
-            alt=""
-            className="object-contain w-full h-full rounded-full "
-          />
-          <div className="absolute bg-[rgba(0,0,0,0.04)] inset-0 rounded-full" />
-          <span>Animalerie</span>
-        </div>
-      </article>
+
+      {categories.map((categorie) => (
+        <article key={categorie.id} className="">
+          <div className="relative w-40 h-40 text-center">
+            <img
+              src={`../src/assets/categories/${categorie.image}`}
+              alt={categorie.title}
+              className="object-contain w-full h-full rounded-full "
+            />
+            <div className="absolute bg-[rgba(0,0,0,0.04)] inset-0 rounded-full" />
+            <span>{categorie.title}</span>
+          </div>
+        </article>
+      ))}
     </div>
   );
 }
