3207638454fb0bedc28e6c4c8e5addfc6b21c15e
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-11 11:21:34 +0200

    ajout background opacity en dehors modal

--- src/components/HomePage/ProductAddForm.tsx
+++ src/components/HomePage/ProductAddForm.tsx
@@ -49,72 +49,104 @@ function ProductAddForm({
   }
 
   return (
-    <div className="col-span-4">
+    <div>
       {displayModal && (
-        <form
-          className="grid grid-cols-2"
-          onSubmit={handleSubmitAddProduct}
-          action=""
-        >
-          <div className="bg-Main_low p-3 col-span-2 grid grid-cols-2">
-            <h3 className="">Ajouter un produit</h3>
-            <div className="justify-self-end">
-              <img src="../../../icons/icons8-close.svg" alt="close button" />
-            </div>
-          </div>
-          <div className="col-span-2 flex flex-col">
-            <label htmlFor="titre">Titre</label>
-            <input
-              className="bg-Main_medium"
-              type="text"
-              name="titre"
-              required
-            />
-          </div>
+        <div className="fixed inset-0 z-50 flex items-center justify-center">
+          {/* Overlay semi-transparent */}
+          <div className="fixed inset-0 bg-black opacity-50 z-40" />
 
-          <div className="col-span-2 flex flex-col">
-            <label htmlFor="url">URL de la photo</label>
-            <input className="bg-Main_medium" type="text" name="url" required />
-          </div>
+          {/* Contenu de la modale */}
+          <div className="relative z-50 col-span-4 w-3/4 justify-self-center">
+            <form
+              className="grid grid-cols-2 gap-2 rounded-md bg-white shadow-lg"
+              onSubmit={handleSubmitAddProduct}
+              action=""
+            >
+              <div className="bg-Main_low p-5 col-span-2 grid grid-cols-2 rounded-t-md border-b border-Brand_grey">
+                <h3 className="font-bold">Ajouter un produit</h3>
+                <div className="justify-self-end">
+                  <button type="button">
+                    <img
+                      className="w-6"
+                      src="../../../icons/icons8-close.svg"
+                      alt="close button"
+                    />
+                  </button>
+                </div>
+              </div>
 
-          <div className="col-span-2 flex flex-col">
-            <label htmlFor="price">Prix (en euros)</label>
-            <input
-              placeholder="90.99"
-              className="bg-Main_medium"
-              type="text"
-              name="price"
-            />
-          </div>
+              <div className="col-span-2 flex flex-col px-4">
+                <label htmlFor="titre">Titre</label>
+                <input
+                  className="border border-Brand_grey rounded-sm px-2 py-1"
+                  type="text"
+                  name="titre"
+                  required
+                />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="categorie">Catégorie</label>
-            <select name="categorie" id="" required>
-              <option value="">Choisissez une catégorie</option>
-              {categories.map((categorie) => (
-                <option key={categorie.id} value={categorie.title}>
-                  {categorie.title}
-                </option>
-              ))}
-            </select>
-          </div>
+              <div className="col-span-2 flex flex-col px-4">
+                <label htmlFor="url">URL de la photo</label>
+                <input
+                  className="border border-Brand_grey rounded-sm px-2 py-1"
+                  type="text"
+                  name="url"
+                  required
+                />
+              </div>
 
-          <div className="flex flex-col">
-            <label htmlFor="tag">Tag</label>
-            <select name="tag" id="">
-              <option value="">Choisissez un tag</option>
-              {tags.map((tag) => (
-                <option key={tag.id} value={tag.text}>
-                  {tag.text}
-                </option>
-              ))}
-            </select>
-          </div>
+              <div className="col-span-2 flex flex-col px-4">
+                <label htmlFor="price">Prix (en euros)</label>
+                <input
+                  placeholder="90.99"
+                  className="border border-Brand_grey rounded-sm px-2 py-1"
+                  type="text"
+                  name="price"
+                />
+              </div>
 
-          <button className="bg-Brand_primary col-span-2" type="submit">
-            Ajouter
-          </button>
-        </form>
+              <div className="flex flex-col gap-1 px-4">
+                <label htmlFor="categorie">Catégorie</label>
+                <select
+                  className="border border-Brand_grey rounded-sm bg-Main_Lowest px-2 py-1"
+                  name="categorie"
+                  id=""
+                  required
+                >
+                  <option value="">Choisissez une catégorie...</option>
+                  {categories.map((categorie) => (
+                    <option key={categorie.id} value={categorie.title}>
+                      {categorie.title}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <div className="flex flex-col gap-1 px-4">
+                <label htmlFor="tag">Tag</label>
+                <select
+                  className="border border-Brand_grey rounded-sm bg-Main_Lowest px-2 py-1"
+                  name="tag"
+                  id=""
+                >
+                  <option value="">Choisissez un tag</option>
+                  {tags.map((tag) => (
+                    <option key={tag.id} value={tag.text}>
+                      {tag.text}
+                    </option>
+                  ))}
+                </select>
+              </div>
+
+              <button
+                className="bg-Button_primary_default_bg border-Button_primary_default_border col-span-2 p-2 rounded-lg mt-2 mx-4 mb-8"
+                type="submit"
+              >
+                Ajouter
+              </button>
+            </form>
+          </div>
+        </div>
       )}
     </div>
   );
