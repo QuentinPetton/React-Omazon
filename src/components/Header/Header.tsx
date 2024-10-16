4d4295ec7a77ff4f550e32e59d7027886bd98307
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-17 00:00:40 +0200

    features: wip scrollShadow

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,7 @@ import HeaderLogo from './HeaderLogo';
 import HeaderForm from './HeaderForm';
 import HeaderMenu from './HeaderMenu';
 import type { Category, Product } from '../../assets/@types';
+import { useCallback, useEffect, useState } from 'react';
 
 type HeaderProps = {
   items: Category[];
@@ -10,8 +11,27 @@ type HeaderProps = {
 };
 
 function Header({ items, cartProducts, products }: HeaderProps) {
+  const [shadowScrollHeader, setShadowScrollHeader] = useState(false);
+
+  // useEffect(() => {
+  //   console.log("il faut mettre en place un écouteur d'événement scroll");
+  // }, []);
+
+  const handleScroll = useCallback(() => {
+    console.log('scroll!');
+    console.log(document.documentElement.scrollTop);
+    setShadowScrollHeader(true);
+  }, []);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    // bonne pratique : enlever l'écouteur d'événement à la destruction du
+    // composant App, avec une fonction de nettoyage
+  }, [handleScroll]);
+
   return (
-    <header className="bg-Main_high text-Main_Lowest p-4">
+    <header className="bg-Main_high text-Main_Lowest p-4 ">
       <div className="flex justify-between items-center">
         <HeaderLogo />
         <HeaderMenu cartProducts={cartProducts} />
