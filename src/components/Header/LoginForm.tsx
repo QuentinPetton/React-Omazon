9787ee85a6394f1452c42f850925a7d0a3c52f31
QuentinPetton <quentin.petton@hotmail.fr>
2024-10-21 11:56:51 +0200

    LoginForm component: mise en place structure du formulaire + todos

--- src/components/Header/LoginForm.tsx
+++ src/components/Header/LoginForm.tsx
@@ -1,7 +1,40 @@
+//todo mise en page et style composant
+//todo affichage conditionnel au clic => useState + onClick
+//todo focus sur input email
+
 function LoginForm() {
   return (
     <div>
-      <p>test</p>
+      <form
+        action=""
+        className="flex
+       flex-col"
+      >
+        <div>
+          <label htmlFor="email">Adresse e-mail</label>
+          <input
+            type="text"
+            name="email"
+            className="border border-Brand_grey rounded-sm px-2 py-1"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="password">Mot de passe</label>
+          <input
+            type="text"
+            name="password"
+            className="border border-Brand_grey rounded-sm px-2 py-1"
+          />
+        </div>
+
+        <button
+          className="bg-Button_primary_default_bg border-Button_primary_default_border col-span-2 p-2 rounded-lg mt-2 mx-4 mb-8"
+          type="submit"
+        >
+          Identifiez-vous
+        </button>
+      </form>
     </div>
   );
 }
